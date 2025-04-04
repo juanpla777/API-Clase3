dfbdf32c671c13347fe1c6554e634fad9769c754
Juan Manuel Pla <jpla@uade.edu.ar>
2025-04-03 21:30:44 -0300

    otro cambio

---
+++ labo_clase04-javascript/llamadaAsyncAwait.js
@@ -0,0 +1,14 @@
+async function getData() {
+    const url = "https://reqres.in/api/users?page=1";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      return json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
